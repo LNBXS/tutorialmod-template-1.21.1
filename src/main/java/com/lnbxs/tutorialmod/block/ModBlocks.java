6e0873777214d47d2d38d0c961593bd8d6054d01
ekem3 <ekem379j@outlook.com>
2025-07-10 18:24:27 +0800

    feat(loot_table): 添加冰以太方块和冰以太矿石的掉落表 feat(blocks): 增加工具需求属性到冰以太方块和矿石 feat(block_tags): 添加冰以太矿石到需要铁工具的标签 feat(block_tags): 添加生冰以太方块到需要石工具的标签 feat(block_tags): 添加冰以太方块、矿石和生冰以太方块到可被镐挖掘的标签 feat(loot_table): 添加生冰以太方块的掉落表 (Loot List   战利品列表)

--- src/main/java/com/lnbxs/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/lnbxs/tutorialmod/block/ModBlocks.java
@@ -11,9 +11,11 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
 
-    public static final Block ICE_ETHER_BLOCK = register("ice_ether_block", new Block(AbstractBlock.Settings.create().strength(3.0f, 3.0f)));
-    public static final Block ICE_ETHER_ORE = register("ice_ether_ore", new Block(AbstractBlock.Settings.create().strength(4.5f, 6.0f)));
-    public static final Block RAW_ICE_ETHER_BLOCK = register("raw_ice_ether_block", new Block(AbstractBlock.Settings.create().strength(3.0f, 3.0f)));
+    public static final Block ICE_ETHER_BLOCK = register("ice_ether_block", new Block(AbstractBlock.Settings.create().requiresTool().strength(3.0f, 3.0f)));
+
+    public static final Block ICE_ETHER_ORE = register("ice_ether_ore", new Block(AbstractBlock.Settings.create().requiresTool().strength(4.5f, 6.0f)));
+
+    public static final Block RAW_ICE_ETHER_BLOCK = register("raw_ice_ether_block", new Block(AbstractBlock.Settings.create().requiresTool().strength(3.0f, 3.0f)));
 
     public static Block register(String id, Block block) {
         registerBlockItems(id, block);
